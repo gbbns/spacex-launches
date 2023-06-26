285c9f23c9161cf31599178c81db804c580c28b4
Chris Gibbons <gbbns@users.noreply.github.com>
2023-06-26 17:59:01 +0100

    feat: breaking launch data down more

--- components/LaunchCard.tsx
+++ components/LaunchCard.tsx
@@ -1,7 +1,36 @@
-export default function LaunchCard() {
+import { Payload } from './Payload';
+export const LaunchCard = ({ launchItem, index }) => {
+  const {
+    id,
+    name,
+    date_utc,
+    cores,
+    success,
+    failures,
+    links: {
+      patch: { small },
+    },
+    payloads,
+  } = launchItem;
   return (
     <>
-      <p>This is a launch card</p>
+      <div key={id} className="card-small">
+        <h2>{name}</h2>
+        <p>
+          {new Date(date_utc).toLocaleDateString('en-GB', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          })}
+        </p>
+        <p>{small}</p>
+        <p>{cores[0].core}</p>
+        <p>{<Payload payloadID={payloads[0]} />}</p>
+
+        <p>
+          {success}-{failures}
+        </p>
+      </div>
     </>
   );
-}
+};
--- components/Launches.tsx
+++ components/Launches.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css';
-import LaunchCard from './LaunchCard';
+import { LaunchCard } from './LaunchCard';
 
 export default function Launches(data: any) {
   return (
@@ -10,19 +10,7 @@ export default function Launches(data: any) {
             .slice(0)
             .reverse()
             .slice(0, 10)
-            .map((launchData: any, index: number) => (
-              <div key={index} className="card-small">
-                <h2>{launchData.name}</h2>
-                <p>
-                  {new Date(launchData.date_utc).toLocaleDateString('en-GB', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric',
-                  })}
-                </p>
-              </div>
-            ))}
-        <LaunchCard />
+            .map((launchData: any, index: number) => <LaunchCard launchItem={launchData} index={index} />)}
       </div>
     </>
   );
